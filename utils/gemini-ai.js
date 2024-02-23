e572ffb11e897b380cf67e12d8be8190c5a90543
Md Monu <mdmonu2021@gmail.com>
2024-02-23 17:23:47 +0530

    Update gemini-ai.js

--- utils/gemini-ai.js
+++ utils/gemini-ai.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 export const GeminiAi = (text) => {
   const genAI = new GoogleGenerativeAI(process.env.gemini_api_key);
 
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
 
   return model.generateContent([text]);
 };

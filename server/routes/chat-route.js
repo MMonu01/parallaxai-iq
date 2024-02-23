ea6e795d373fd7e8c1c4680c3681db36ca525f16
mohammad.monu <mohammad.monu@healthmug.com>
2024-02-23 23:37:25 +0530

    gemini ai changes

--- server/routes/chat-route.js
+++ server/routes/chat-route.js
@@ -1,8 +1,6 @@
 import express from "express";
-import markdownit from "markdown-it";
 
 import { chatModel, messageModel } from "../models/chat-model.js";
-import { GeminiAi } from "../utils/gemini-ai.js";
 
 export const chatRouter = express.Router();
 
@@ -45,37 +43,42 @@ chatRouter.post("/chat-response", (req, res, next) => {
   let is_valid_chat_id = false;
 
   try {
-    GeminiAi(question).then(async (result) => {
-      const md = markdownit();
-      const response = md.render(result.response.text());
+    fetch(process.env.GEMINI_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ question }),
+    })
+      .then((res) => (res.ok ? res.json() : next(res)))
+      .then(async (result) => {
+        const response = result.response;
 
-      if (!req.userId) {
-        res.status(200).send({ chatId: null, message: { _id: null, question: question, answer: response } });
-      } else {
-        if (chatId) {
-          const chat = await chatModel.findOne({ _id: chatId });
-          if (chat) {
-            is_valid_chat_id = true;
-            current_chat_id = chat._id;
-          }
-        }
-
-        if (is_valid_chat_id) {
-          const chat = await chatModel.updateOne({ _id: current_chat_id }, { preview: question });
+        if (!req.userId) {
+          res.status(200).send({ chatId: null, message: { _id: null, question: question, answer: response } });
         } else {
-          const new_chat = new chatModel({ userId, preview: question, date: new Date() });
-          await new_chat.save();
-          current_chat_id = new_chat._id;
+          if (chatId) {
+            const chat = await chatModel.findOne({ _id: chatId });
+            if (chat) {
+              is_valid_chat_id = true;
+              current_chat_id = chat._id;
+            }
+          }
+
+          if (is_valid_chat_id) {
+            const chat = await chatModel.updateOne({ _id: current_chat_id }, { preview: question });
+          } else {
+            const new_chat = new chatModel({ userId, preview: question, date: new Date() });
+            await new_chat.save();
+            current_chat_id = new_chat._id;
+          }
+
+          const new_message_obj = { chatId: current_chat_id, userId, date: new Date(), type: "text", question, answer: response };
+
+          const new_message = new messageModel(new_message_obj);
+          await new_message.save();
+
+          res.status(200).send({ chatId: current_chat_id, message: { _id: new_message._id, question: new_message.question, answer: new_message.answer } });
         }
-
-        const new_message_obj = { chatId: current_chat_id, userId, date: new Date(), type: "text", question, answer: response };
-
-        const new_message = new messageModel(new_message_obj);
-        await new_message.save();
-
-        res.status(200).send({ chatId: current_chat_id, message: { _id: new_message._id, question: new_message.question, answer: new_message.answer } });
-      }
-    });
+      });
   } catch (err) {
     next(err);
   }
